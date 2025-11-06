09ebeb7506c7eaae14736e2435ba07548733c556
Veit Ulrich <veit.ulrich@heigit.org>
2025-11-06 15:09:14 +0000

    chore(create_emissions_raster_data): Remove unused function create_emissions_raster_data in utils.py (#42)
    Closes #42

--- heating_emissions/components/utils.py
+++ heating_emissions/components/utils.py
@@ -1,18 +1,7 @@
 import logging
 from enum import StrEnum
-from itertools import product
-from typing import Tuple
 
 import geopandas as gpd
-import matplotlib as mpl
-import numpy as np
-import pandas as pd
-import xarray
-from climatoology.base.artifact import RasterInfo
-from pydantic_extra_types.color import Color
-from rasterio import CRS
-from rasterio._base import Affine
-from rasterio.warp import reproject
 
 log = logging.getLogger(__name__)
 
@@ -73,19 +62,6 @@ class Topics(StrEnum):
     PARAMETERS = 'parameters'
 
 
-def generate_colors(color_by: pd.Series, cmap_name: str, cap: float = 1.0) -> dict[int : tuple[int, int, int]]:
-    norm = mpl.colors.Normalize(vmin=0, vmax=color_by.quantile(cap))
-    cmap = mpl.colormaps[cmap_name]
-    mapped_colors = {}
-    for value in range(0, int(color_by.quantile(cap)) + 1):
-        mapped_colors[value] = Color(mpl.colors.to_hex(cmap(norm(value)))).as_rgb_tuple()
-
-    for value in range(int(color_by.quantile(cap)) + 1, int(color_by.max()) + 1):
-        mapped_colors[value] = Color(mpl.colors.to_hex(cmap(norm(int(color_by.quantile(cap)) + 1)))).as_rgb_tuple()
-
-    return mapped_colors
-
-
 def get_aoi_area(aoi_as_geoseries: gpd.GeoSeries) -> float:
     reprojected_aoi_df = aoi_as_geoseries.to_crs(aoi_as_geoseries.estimate_utm_crs())
     area_km2 = round(reprojected_aoi_df.geometry.area.sum() / 1e6, 2)
@@ -115,73 +91,3 @@ def postprocess_uncalculate_census_data(uncalculated_census_data: gpd.GeoDataFra
         ['dominant_age', 'dominant_energy']
     ].fillna('Unknown')
     return uncalculated_census_data
-
-
-def create_emissions_raster_data(  # dead: disable
-    result: gpd.GeoDataFrame, cmap: str, output_emissions: str, cap: float = 1.0
-) -> RasterInfo:
-    grid_size = 100  # m as of EPSG:3035
-    geom_in_3035 = result.geometry.to_crs(3035)
-    result = result.assign(x_mp_100m=geom_in_3035.x.astype(int), y_mp_100m=geom_in_3035.y.astype(int))
-    raster_df = result.set_index(['y_mp_100m', 'x_mp_100m'])
-    raster_df = raster_df[[output_emissions]].round()
-
-    # pad dataframe by missing grid cells
-    all_x = [x for x in range(result['x_mp_100m'].min(), result['x_mp_100m'].max() + grid_size, grid_size)]
-    all_y = [y for y in range(result['y_mp_100m'].min(), result['y_mp_100m'].max() + grid_size, grid_size)]
-    missing_x = set(all_x).difference(set(result['x_mp_100m']))
-    missing_y = set(all_y).difference(set(result['y_mp_100m']))
-    missing_pairs = list(product(missing_y, missing_x))
-
-    if len(missing_pairs) != 0:
-        for index in missing_pairs:
-            raster_df.loc[index, output_emissions] = 0
-    elif len(missing_y) == 0:
-        for x in missing_x:
-            raster_df.loc[(all_y[0], x), output_emissions] = 0
-    elif len(missing_x) == 0:
-        for y in missing_y:
-            raster_df.loc[(y, all_x[0]), output_emissions] = 0
-
-    raster_df.sort_index(level=[1, 0], inplace=True)
-
-    raster_data = raster_df[output_emissions].to_xarray()
-
-    transform = Affine(
-        grid_size,
-        0.0,
-        result['x_mp_100m'].min() - (grid_size / 2),
-        0.0,
-        grid_size,
-        result['y_mp_100m'].min() - (grid_size / 2),
-    )
-
-    projected_raster_data, projected_transform = reproject_data_to_4326(raster_data, transform, CRS.from_epsg(3035))
-    color_map = generate_colors(color_by=result[output_emissions], cmap_name=cmap, cap=cap)
-
-    raster_info = RasterInfo(
-        data=projected_raster_data.astype(np.uint16),
-        crs=CRS.from_epsg(4326),
-        transformation=projected_transform,
-        colormap=color_map,
-    )
-
-    return raster_info
-
-
-def reproject_data_to_4326(
-    source_data: xarray.DataArray, source_transform: Affine, source_crs: CRS
-) -> Tuple[
-    np.ndarray,
-    Affine,
-]:
-    destination = np.zeros(source_data.shape, np.int16)
-    geographic_raster_data, geographic_transform = reproject(
-        source=source_data.to_numpy(),
-        destination=destination,
-        src_transform=source_transform,
-        src_crs=source_crs,
-        dst_crs=CRS.from_epsg(4326),
-    )
-    log.debug(f'projected transform: {geographic_transform}')
-    return geographic_raster_data, geographic_transform
--- test/components/test_utils.py
+++ test/components/test_utils.py
@@ -1,14 +1,10 @@
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-from climatoology.base.artifact import RasterInfo
 from shapely import box
-from shapely.geometry import Point
 
 from heating_emissions.components.utils import (
     calculate_heating_emissions,
-    create_emissions_raster_data,
-    generate_colors,
     get_aoi_area,
     postprocess_uncalculate_census_data,
 )
@@ -71,40 +67,6 @@ def test_calculate_heating_emissions_missing_data():
     assert result['co2_emissions'][0] == expected_absolute
 
 
-def test_generate_colors():
-    test_data = pd.Series(data=[1, 2, 4])
-
-    expected_color_map = {0: (59, 76, 192), 1: (141, 176, 254), 2: (221, 220, 220), 3: (244, 152, 122), 4: (180, 4, 38)}
-
-    resulting_color_map = generate_colors(test_data, cmap_name='coolwarm')
-
-    assert isinstance(resulting_color_map[0], tuple)
-    assert isinstance(resulting_color_map[3], tuple)
-    assert isinstance(resulting_color_map[4], tuple)
-
-    assert resulting_color_map == expected_color_map
-
-
-def test_generate_colors_triggers_max_branch():
-    color_by = pd.Series([1, 2, 10])
-    expected_color_map = {
-        0: (59, 76, 192),
-        1: (111, 146, 243),
-        2: (170, 199, 253),
-        3: (221, 220, 220),
-        4: (247, 184, 156),
-        5: (231, 116, 91),
-        6: (180, 4, 38),
-        7: (180, 4, 38),
-        8: (180, 4, 38),
-        9: (180, 4, 38),
-        10: (180, 4, 38),
-    }
-    resulting_color_map = generate_colors(color_by, cmap_name='coolwarm', cap=0.75)
-
-    assert resulting_color_map == expected_color_map
-
-
 def test_get_aoi_area():
     aoi = gpd.GeoSeries(box(0, 0, 1000, 1000), crs='EPSG:3857')
     area = get_aoi_area(aoi)
@@ -122,16 +84,3 @@ def test_postprocess_uncalculate_census_data():
     result = postprocess_uncalculate_census_data(df)
     assert all(result['dominant_age'] == ['Unknown', '1949-1978', 'Unknown'])
     assert all(result['dominant_energy'] == ['Unknown', 'Gas', 'Unknown'])
-
-
-def test_create_emissions_raster_data():
-    # Create a minimal GeoDataFrame with two points and emissions
-    df = gpd.GeoDataFrame(
-        {'geometry': [Point(10, 50), Point(10.001, 50.001)], 'emissions': [100, 200]}, crs='EPSG:4326'
-    )
-    raster_info = create_emissions_raster_data(df, cmap='viridis', output_emissions='emissions', cap=1.0)
-    assert isinstance(raster_info, RasterInfo)
-    assert raster_info.data is not None
-    assert raster_info.data.dtype == np.uint16
-    assert raster_info.colormap is not None
-    assert raster_info.crs.to_epsg() == 4326
