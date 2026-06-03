3a85db2037a383c86de4c13520a51feb7d492287
Moritz Schott <moritz.schott@heigit.org>
2026-06-03 13:21:14 +0000

    refactor(settings): accept connection parts instead of full url

--- heating_emissions/core/settings.py
+++ heating_emissions/core/settings.py
@@ -3,12 +3,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     log_level: str = 'INFO'
-    ca_database_url: str
+
+    data_db_host: str
+    data_db_port: int
+    data_db_name: str
+    data_db_user: str
+    data_db_password: str
 
     cdsapi_url: str = 'https://cds.climate.copernicus.eu/api'
     cdsapi_key: str = None
 
     model_config = SettingsConfigDict(env_file='.env')  # dead: disable
+
+    @property
+    def ca_database_url(self) -> str:
+        return f'postgresql://{self.data_db_user}:{self.data_db_password}@{self.data_db_host}:{self.data_db_port}/{self.data_db_name}'
 
 
 class FeatureFlags(BaseSettings):
